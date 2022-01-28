bce2a70df4616e9f669b2dbb10a4dc8dcab21246
arshpals <arshpals@sheridancollege.ca>
2022-01-28 09:49:46 -0500

    First Commit

---
+++ src/studentlist/project/StudentListProject.java
@@ -0,0 +1,19 @@
+package studentlist.project;
+
+/**
+ *This class +++ Insert description Here+++ 
+ *
+ * @author Arshpal Singh
+ */
+public class StudentListProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("my name is arshpal singh");
+        System.out.println("  helooo! arsh!");
+    }
+
+}
